b3fd71f49a131eed039df7064e15cc5058171aab
JZ52 <zaletskij.jenya@gmail.com>
2020-02-27 22:03:24 +0200

    Need end cart

--- _extention/cart.php
+++ _extention/cart.php
@@ -1,13 +1,3 @@
 <?php
-    session_start();
-
-    // ID товара
-    $_SESSION['id'] = $_POST['id'];
-    // Кол-во товара
-    $_SESSION['count'] = $_POST['count'];
-
-    if(isset($_SESSION['id']) and is_numeric($_SESSION['count'])){
-        echo $_SESSION['id'];
-        echo $_SESSION['count'];
-    }
+    $_SESSION =
 ?>
