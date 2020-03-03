3a4eb3d7eaa7ceb19a9282eb9f2a24c61bc412b7
Eugen <36745162+JZ52@users.noreply.github.com>
2020-03-03 21:03:41 +0200

    Update cart.php

--- _extention/cart.php
+++ _extention/cart.php
@@ -1,3 +1,15 @@
-<?php
-    $_SESSION =
+<?
+
+$_SESSION['id'] = 0;
+$_SESSION['count'] = 0;
+
+$sql = "SELECT * FROM record WHERE id";
+
+foreach($id as $item) {
+    
+    $sum = $item * $count;
+ 
+
+}
+
 ?>
